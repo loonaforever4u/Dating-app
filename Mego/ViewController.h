d0906e92b393681bc3c6807ec90e6715cc4e84d8
Loona Marianne <loonamarianne@Loonas-Mac.local>
2015-03-16 16:15:49 +0100

    Initial Commit

---
+++ Mego/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mego
+//
+//  Created by Loona Marianne on 16/03/15.
+//  Copyright (c) 2015 Loona Marianne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mego/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Mego
+//
+//  Created by Loona Marianne on 16/03/15.
+//  Copyright (c) 2015 Loona Marianne. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
